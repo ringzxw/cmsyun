7c9914008e675f5f4d11589c7f38959f842f3222
ring0531 <119828376@qq.com>
2018-11-02 10:58:05 +0800

    初始化

---
+++ app/Admin/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\Dashboard;
+use Encore\Admin\Layout\Column;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Layout\Row;
+use Encore\Admin\Facades\Admin;
+
+class HomeController extends Controller
+{
+    public function index(Content $content)
+    {
+        return Admin::content(function (Content $content) {
+            $content->header('查看订单');
+            // body 方法可以接受 Laravel 的视图作为参数
+            $content->body(view('admin.security.index'));
+        });
+    }
+}
---
+++ resources/views/admin/security/index.blade.php
@@ -0,0 +1,38 @@
+<link rel="stylesheet" href="{{ admin_asset("/blue_sky/css/style.css") }}">
+<link rel="stylesheet" href="{{ admin_asset("/blue_sky/css/bootstrap-bluesky.css") }}">
+<link rel="stylesheet" href="{{ admin_asset("/blue_sky/css/checkbox.css") }}">
+<link rel="stylesheet" href="{{ admin_asset("/blue_sky/css/flow-site.css") }}">
+<link rel="stylesheet" href="{{ admin_asset("/blue_sky/css/icon.css") }}">
+<link rel="stylesheet" href="{{ admin_asset("/blue_sky/css/site.css") }}">
+<link rel="stylesheet" href="{{ admin_asset("/blue_sky/css/idangerous.swiper.css") }}">
+<link rel="stylesheet" href="{{ admin_asset("/blue_sky/css/icon-dialog.css") }}">
+<link rel="stylesheet" href="{{ admin_asset("/blue_sky/css/icon-custom.css") }}">
+
+<link rel="stylesheet" href="{{ admin_asset("/welfarecorebackend/default/css/common.cs") }}">
+<link rel="stylesheet" href="{{ admin_asset("/blue_sky/css/icon-custom.css") }}">
+<link rel="stylesheet" href="{{ admin_asset("/blue_sky/css/icon-custom.css") }}">
+<link rel="stylesheet" href="{{ admin_asset("/blue_sky/css/icon-custom.css") }}">
+
+
+<div class="item">
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">
+                <ul class="breadcrumbs">
+                    <li><a>角色设置</a></li>
+                    <li><a href="#">设置权限（管理员）</a></li>
+                </ul>
+            </h3>
+        </div>
+        <div class="box-body">
+            <div class="right-tools">
+                <h4>管理员</h4>
+                <a class="btn-trans all-role">启用全部功能</a>
+                <div class="pull-right">
+                    <span class="warning"><i class="fa fa-exclamation-circle"></i>所有修改操作保存设置后生效</span>
+                    <button class="btn btn-warning save-role">保存设置</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
